4aa8eedfbb50ad1268416462a05a1ddf0c51cb68
Rudra-Singh-12 <rudrapratapsingh0412@gmail.com>
2025-07-10 12:29:12 +0530

    first bit program in java

--- src/Bit_Manipulation/BitsInjava.java
+++ src/Bit_Manipulation/BitsInjava.java
@@ -2,6 +2,10 @@ package Bit_Manipulation;
 
 public class BitsInjava {
     public static void main(String[] args) {
-
+        System.out.println((5 & 6)); // Bitwise And Operator
+        System.out.println((5|6)); //Bitwise Or Operator
+        System.out.println((~5)); // Bitwise Not operator
+        System.out.println((5<<2)); // bitwise left shift operator
+        System.out.println((5>>2)); // bitwise right shift operator
     }
 }
